8936f9fdf5fdc5159c472e99ab4447a6ee524f49
Gian <gmicheloni52@gmail.com>
2025-12-04 10:48:42 -0300

    inicio app - primeras pantallas

---
+++ src/main/java/com/mycompany/tpintegrador/TpIntegrador.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tpintegrador;
+
+import com.mycompany.tpintegrador.vista.Entrada;
+
+
+
+/**
+ *
+ * @author gian_
+ */
+public class TpIntegrador {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        new Entrada().setVisible(true);
+        
+       
+        
+    }
+}
